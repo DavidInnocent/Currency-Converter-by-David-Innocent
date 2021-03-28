81bad7e90679ebff5f893910d72ddc729c22fbbb
David Innocent <davidinnocent@live.com>
2021-03-28 15:38:25 +0300

    created ui for the converter

--- app/src/main/java/com/david/currencyconverter/ui/main/MainActivity.kt
+++ app/src/main/java/com/david/currencyconverter/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.david.currencyconverter
+package com.david.currencyconverter.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.david.currencyconverter.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
