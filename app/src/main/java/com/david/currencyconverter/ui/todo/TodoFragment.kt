bb9a565fc39755658bca84d800d333a1a26ea426
David Innocent <davidinnocent@live.com>
2021-03-28 16:15:44 +0300

    added hilt dependencies

--- app/src/main/java/com/david/currencyconverter/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/david/currencyconverter/ui/todo/TodoFragment.kt
@@ -11,9 +11,6 @@ import android.view.ViewGroup
 import com.david.currencyconverter.databinding.FragmentTodoBinding
 
 
-/**
- * A fragment representing a list of Items.
- */
 class TodoFragment : Fragment() {
 
     lateinit var fragmentTodoBinding: FragmentTodoBinding;
