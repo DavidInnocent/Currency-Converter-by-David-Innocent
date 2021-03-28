c4e07ca86d56aedadee3ece16c7c7b43ae1c23ba
David Innocent <davidinnocent@live.com>
2021-03-28 16:16:04 +0300

    added hilt dependencies

---
+++ app/src/main/java/com/david/currencyconverter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.david.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.david.currencyconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
